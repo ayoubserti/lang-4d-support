531e933318e0210e1fd15bf48a3a042dfca8041a
Ayoub Serti <ayb.serti@gmail.com>
2019-10-14 16:34:23 +0100

    wip: build task

--- src/templating.ts
+++ src/templating.ts
@@ -1,4 +1,8 @@
 'use strict';
+import * as vscode from 'vscode';
+import {resolve} from 'path';
+const config = vscode.workspace.getConfiguration("4d");
+
 
 export namespace content {
 
@@ -8,26 +12,19 @@ export namespace content {
             {
                 // See https://go.microsoft.com/fwlink/?LinkId=733558
                 // for the documentation about the tasks.json format
-                "version": "2.0.0",
-                "tasks": [
-                    {
-                        "label": "Build",
-                        "type": "shell",
-                        "group": "build",
-                        "osx": {
-                            "command": "/Users/mac/Desktop/tool4d.app/Contents/MacOS/4D", //TODO: to be changed
-                            "args": [
-                                "--headless",
-                                "-s",
-                                "${workspaceFolder}/Project/builder.4DProject",
-                                "--data-less",
-                                "--user-data",
-                                "{\"makeFile\":\"${workspaceFolder}/make.json\",\"verbose\":true,\"config\":\"release\"}"
-                            ]
-                        },
-                        "problemMatcher": []
-                    }
-                ]
+                "label": "Build",
+                "type": "shell",
+                "group": "build",
+                "command": config.get("programPath"),
+                "args": [
+                    "--headless",
+                    "-s",
+                    config.get("builderPath"),
+                    "--dataless",
+                    "--user-param",
+                    "{\\\"makeFile\\\":\\\"${workspaceFolder}\\make.json\\\",\\\"verbose\\\":true,\\\"config\\\":\\\"release\\\"}"
+                ],
+                "problemMatcher": ["$msCompile"]
             }
 
         ]
@@ -38,9 +35,9 @@ export namespace content {
         version: '0.2.0',
         configuration: [
             {
-                name: "Compile Project",
-                type: "4d",
-                request: "launch",
+                "name": "Compile Project",
+                "type": "4d",
+                "request": "launch",
                 "runtimeExecutable": "${execPath}",
                 "args": [
                     "--extensionDevelopmentPath=${workspaceFolder}"
@@ -56,6 +53,7 @@ export namespace content {
     export const dir: any = ['Project', 'Resources', 'Project/Sources/', 'Project/Sources/Methods', 'Project/Sources/DatabaseMethods', 'Settings'];
     export const files: any = [
         { source: "_.4DProject.tmpl", target: 'Project/<name>.4DProject', changeName:true },
+        { source: "make.json.tmpl", target:  'make.json' },
         { source: "catalog.4DCatalog.tmpl", target: 'Project/Sources/catalog.4DCatalog' },
         { source: "folders.json.tmpl", target:  'Project/Sources/folders.json' },
         { source: "menus.json.tmpl", target:  'Project/Sources/menus.json' },
