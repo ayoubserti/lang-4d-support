e2df35ceb45273dcfb5307acbdc53f615822e1b9
Ayoub Serti <ayb.serti@gmail.com>
2019-10-17 16:14:02 +0100

    webpack  and prepare to publish

--- tools/postbuild.js
+++ tools/postbuild.js
@@ -5,28 +5,28 @@ const { copyFile, existsSync, mkdirSync,readdir } = require('fs');
 const copyFile$ = promisify(copyFile);
 const readdir$  = promisify(readdir);
 (async () => {
-    if (!existsSync( resolve(__dirname, '../out/syntaxes'))){
-        mkdirSync(resolve(__dirname, '../out/syntaxes'));
+    if (!existsSync( resolve(__dirname, '../dist/syntaxes'))){
+        mkdirSync(resolve(__dirname, '../dist/syntaxes'));
     }
-    if (!existsSync( resolve(__dirname, '../out/support'))){
-        mkdirSync(resolve(__dirname, '../out/support'));
+    if (!existsSync( resolve(__dirname, '../dist/support'))){
+        mkdirSync(resolve(__dirname, '../dist/support'));
     }
     await copyFile$(
         resolve(__dirname, '../syntaxes/4d.tmLanguage.json'),
-        resolve(__dirname, '../out/syntaxes/4d.tmLanguage.json'),
+        resolve(__dirname, '../dist/syntaxes/4d.tmLanguage.json'),
     );
     await copyFile$(
         resolve(__dirname, '../support/commands.txt'),
-        resolve(__dirname, '../out/support/commands.txt'),
+        resolve(__dirname, '../dist/support/commands.txt'),
     );
     
     let source_path = resolve(__dirname, '../template/')
     let files = await readdir$(source_path);
-    if (!existsSync( resolve(__dirname, '../out/template'))){
-        mkdirSync(resolve(__dirname, '../out/template'));
+    if (!existsSync( resolve(__dirname, '../dist/template'))){
+        mkdirSync(resolve(__dirname, '../dist/template'));
     }
     files.forEach(async (elm)=>{
-        await copyFile$(resolve(source_path,elm), resolve(__dirname,'../out/template/',elm));
+        await copyFile$(resolve(source_path,elm), resolve(__dirname,'../dist/template/',elm));
     });
     
 })();
